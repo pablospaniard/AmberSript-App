a61c6531cef6dccb390478db81b28a9a98adabe1
Pavel Ivanov <pablospaniard@gmail.com>
2018-09-06 18:46:47 +0200

    wip

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import { Content } from './Content'
 import { Header } from './Header'
 import { Layout } from './Layout'
-import { List } from './List'
 import { Item } from './Item'
+import { List } from './List'
 
 export { Content, Header, Layout, List, Item }
